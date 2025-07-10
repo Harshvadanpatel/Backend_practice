33f73e04e7cbd1ea776368db750bdd1524ea7c68
HARSHVADAN V PATEL <harshvadanvpatel2005@gmail.com>
2025-07-10 22:29:20 +0530

    practice

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is runing at port :${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!",err)
+  })
 
 
 
