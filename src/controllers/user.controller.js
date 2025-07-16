9a61e2d9fc267a94d63546e8bd0a1a912b70f6d3
HARSHVADAN V PATEL <harshvadanvpatel2005@gmail.com>
2025-07-16 22:22:30 +0530

    minor bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,7 +40,7 @@ const registerUser = asyncHandler( async(req,res)=>{
     
 
     const existedUser = User.findOne({
-        $or:[{username},{email}]
+        $or:[{username},{email} ]
     })
     if(existedUser){
         throw new ApiError(409,"User with email or username already exists")
