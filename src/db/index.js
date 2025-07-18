2c8dd078c3c7a14455adac8dfeca36e637ec4f85
HARSHVADAN V PATEL <harshvadanvpatel2005@gmail.com>
2025-07-18 22:24:47 +0530

    .

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,7 @@ import {DB_NAME} from "../constants.js"
 
 const connectDB = async()=>{
     try {
+            
         const connectinInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
 
         console.log(`\n mongodb connected db host:${connectinInstance.connection.host}`);
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -58,11 +58,6 @@ export {uploadOnCloudinary}
 //   api_secret: process.env.CLOUDINARY_API_SECRET,
 // });
 
-// console.log("CLOUDINARY CONFIG:", {
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
 
 // const uploadOnCloudinary = async (localFilePath) => {
 //   try {
